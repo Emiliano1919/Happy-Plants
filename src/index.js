09828e9ff50544df90be5f243cf02538c654d429
Emiliano1919 <emilianojg38@gmail.com>
2023-11-11 18:48:38 -0500

    Add location of bug

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ buttons.forEach(button => {
         let newIndex = [...slides.children].indexOf(activeSlide)+offset
         if(newIndex<0) newIndex = slides.children.length -1
         if(newIndex >= slides.children.length) newIndex=0
-
+        // Problem here
         slides.children[newIndex].dataset.active = true
         delete activeSlide.dataset.active
         })
